269762212dd54819373cbf819916170d000b2ceb
aser <aser@NOOB-KING>
2023-04-17 01:44:10 +0300

    graf99

---
+++ graflogo99.py
@@ -0,0 +1,39 @@
+from graphics import *
+
+win = GraphWin("noob king", 600, 600)
+
+# Circle large
+Circle(Point(200, 200), 50).draw(win).setWidth(5)
+Circle(Point(400, 200), 50).draw(win).setWidth(5)
+Circle(Point(200, 400), 50).draw(win).setWidth(5)
+Circle(Point(400, 400), 50).draw(win).setWidth(5)
+Circle(Point(150, 300), 50).draw(win).setWidth(5)
+Circle(Point(450, 300), 50).draw(win).setWidth(5)
+Circle(Point(300, 150), 50).draw(win).setWidth(5)
+Circle(Point(300, 450), 50).draw(win).setWidth(5)
+Circle(Point(300, 300), 200).draw(win).setWidth(5)
+
+Line(Point(200, 250), Point(300, 300)).draw(win).setArrow("first")
+Line(Point(400, 250), Point(300, 300)).draw(win).setArrow("first")
+Line(Point(200, 350), Point(300, 300)).draw(win).setArrow("first")
+Line(Point(400, 350), Point(300, 300)).draw(win).setArrow("first")
+
+
+Line(Point(200, 300), Point(400, 300)).draw(win).setArrow("both")
+Line(Point(300, 200), Point(300, 400)).draw(win).setArrow("both")
+
+
+# Circle min
+Circle(Point(200, 200), 25).draw(win).setWidth(5)
+Circle(Point(200, 200), 25).draw(win).setWidth(5)
+Circle(Point(400, 200), 25).draw(win).setWidth(5)
+Circle(Point(200, 400), 25).draw(win).setWidth(5)
+Circle(Point(400, 400), 25).draw(win).setWidth(5)
+Circle(Point(150, 300), 25).draw(win).setWidth(5)
+Circle(Point(450, 300), 25).draw(win).setWidth(5)
+Circle(Point(300, 150), 25).draw(win).setWidth(5)
+Circle(Point(300, 450), 25).draw(win).setWidth(5)
+
+
+win.getMouse()
+win.close()
--- inp.py
+++ inp.py
@@ -1,13 +1,11 @@
 import re
-
-
 id = "([a-zA-Z_][a-zA-Z_[0-9]*)"
 op = r'((==)|(!=)|(>)|(<)|(>=)|(<=))'
 number = '([0-9]+)'
-# 'operation'
 pa = r"((\+\+)|(\-\-))"
 pa2 = r"((\+\=)|(\-\=)|(\*\=)|(\/\=))"
 pa3 = r"((\+)|(\-)|(\*)|(\/))"
+
 
 firstin = rf"({id}\s*\=\s*({id}\s*|{number}\s*))"
 
@@ -18,22 +16,8 @@ third = rf"(\s*{id}\s*{pa2}\s*{number}\s*)|(\s*{id}\s*{pa2}\s*{id}\s*)|(\s*{id}\
 
 barc = '(\s*{\s*.*\s*\}\s*)'
 
-finalform = rf'(for\s*\(\s*({firstin})\s*\;\s*({second})\s*\;\s*({third})\s*\){barc})'
+finalform = rf'(for\s*\(\s*({firstin})\s*\;\s*({second})\s*\;\s*({third})\s*\){barc})'hird})\s*\){barc})'
 
-# print(re.match(finalform,"for(i=0;i<10;i++){}"))
 idop = rf"({id}|{number})"
 
 colla = rf"(\s*{idop}\s*{op}\s*{idop}\s*)"
-
-
-whiles = r"(while\s*\("+colla+"\s*\)"+barc+")"
-
-while2 = rf"while\s*\({colla}\s*\)"
-do1 = rf"(do\s*{barc}\s*{while2}\s*\;)"
-
-
-looping = rf"({finalform})|({whiles})|({do1})"
-
-pattern = re.compile(looping)
-
-print(pattern.match("do{nasnd}while(i<10);"))
